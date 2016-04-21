6e4b89092f9543b3132277552c88592e2b349359
DBKV <donaldb.kennedy@gmail.com>
2016-04-21 16:25:39 -0400

    Autocomplete Added
    Added autocomplete functionality to code window. Currently activates on
    pressing Ctrl and just reads from a short test list of strings, can
    substitute with an array of c# strings later. Need to make it match
    against what is already partially typed.

--- Cts.csw/Cts.csw/App_Start/BundleConfig.cs
+++ Cts.csw/Cts.csw/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace Cts.csw
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/show-hint.css"));
         }
     }
 }

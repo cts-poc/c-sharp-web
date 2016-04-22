5b1e101784d97b8f1a550401a1a4edfef3d631f3
DBKV <donaldb.kennedy@gmail.com>
2016-04-22 14:39:57 -0400

    Static Method Suggestion
    Have successfully loaded methods of a class as a text suggestion as a
    '.' is typed. Currently hardcoded to show the methods of Console class.
    Next step is to make the class type dynamic.

---
+++ Cts.csw/Cts.csw/Models/Intelliscents.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cts.csw.Models
+{
+    // Hmmmmm, smells vaguely of Intellisense...
+    public class Intelliscents
+    {
+        public static List<String> ShowMethods(Type type)
+        {
+            //List<String> output = new List<String>();
+
+            var output = type.GetMethods().OrderBy(method => method.Name).GroupBy(method => method.Name).Select(group => group.First().Name).ToList();
+
+            //foreach (var method in type.GetMethods().GroupBy(method => method.Name).Select(group => group.First()))
+            //{
+            //    var parameters = method.GetParameters();
+            //    var parameterDescriptions = string.Join
+            //        (", ", method.GetParameters()
+            //                     .Select(x => x.ParameterType + " " + x.Name)
+            //                     .ToArray());
+
+            //    output.Add(String.Format("{0}", method.Name));
+            //}
+
+            return output;
+        }
+    }
+}

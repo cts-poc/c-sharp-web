f2eda6b60d26bcf3ebb206cbe2b098f7e3d0610b
DBKV <donaldb.kennedy@gmail.com>
2016-04-28 10:10:03 -0400

    Intellisense Progress
    May be the correct path to the intellisense we need. Still small
    experiments, but will structure the solution once I have proven it to
    myself.

--- Cts.csw/Cts.csw/Models/Intelliscents.cs
+++ Cts.csw/Cts.csw/Models/Intelliscents.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 
 namespace Cts.csw.Models
@@ -8,22 +10,39 @@ namespace Cts.csw.Models
     // Hmmmmm, smells vaguely of Intellisense...
     public class Intelliscents
     {
-        public static List<String> ShowMethods(Type type)
+        public static List<String> ShowMethods(String typeString)
         {
-            //List<String> output = new List<String>();
+            //var types = assembly.GetTypes();
+            Type type = null;
+            List<String> output = new List<String>();
+            string assemblyPath = Path.GetDirectoryName(typeof(object).Assembly.Location);
 
-            var output = type.GetMethods().OrderBy(method => method.Name).GroupBy(method => method.Name).Select(group => group.First().Name).ToList();
+            Assembly[] assemblies = new Assembly[]
+            {
+                Assembly.LoadFrom(Path.Combine(assemblyPath, "mscorlib.dll"))
+                //MetadataReference.CreateFromFile(typeof(object).Assembly.Location),
+                //MetadataReference.CreateFromFile(typeof(Enumerable).Assembly.Location),
+                //MetadataReference.CreateFromFile(typeof(System.Object).Assembly.Location),
+                //MetadataReference.CreateFromFile(typeof(System.String).Assembly.Location),
+                //MetadataReference.CreateFromFile(Path.Combine(assemblyPath, "mscorlib.dll")),
+                //MetadataReference.CreateFromFile(Path.Combine(assemblyPath, "System.dll")),
+                //MetadataReference.CreateFromFile(Path.Combine(assemblyPath, "System.Core.dll")),
+                //MetadataReference.CreateFromFile(Path.Combine(assemblyPath, "System.IO.dll")),
+                //MetadataReference.CreateFromFile(Path.Combine(assemblyPath, "System.Runtime.dll"))
+            };
 
-            //foreach (var method in type.GetMethods().GroupBy(method => method.Name).Select(group => group.First()))
-            //{
-            //    var parameters = method.GetParameters();
-            //    var parameterDescriptions = string.Join
-            //        (", ", method.GetParameters()
-            //                     .Select(x => x.ParameterType + " " + x.Name)
-            //                     .ToArray());
 
-            //    output.Add(String.Format("{0}", method.Name));
-            //}
+            //myType = AppDomain.CurrentDomain.GetAssemblies().Get
+
+            foreach (var assembly in assemblies)
+            {
+                type = assembly.GetTypes().Where(t => t.Name == typeString).First();
+                if (type != null)
+                {
+                    output = type.GetMethods().OrderBy(method => method.Name).GroupBy(method => method.Name).Select(group => group.First().Name).ToList();
+                    break;
+                }
+            }
 
             return output;
         }
